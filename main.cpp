d4904cb40590a7fa0cc8964424b46fc2d5fa0c81
Tiollit <Tiollit7@gmail.com>
2022-10-24 08:44:20 -0700

    Lab 22 done

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     srand(time(0));
     const int a = 4;
     const int b = 5;
-    int arr[a][b]; int arr2[a][b];
+    int arr[a][b];
+    int arr2[a][b];
     int c, d, f;
     for (int i = 0;i < a;i++)
     {
@@ -28,19 +29,18 @@ int main()
         for (int j = 0;j < b;j++)
         {
             d = j + c;
-                while (d > b)
+                while (d >= b)
                 {
                     d = d - b;
                 }                
-                arr2[i][d] = arr[i][j];
-                cout << arr2[i][d] << " <- " << arr[i][j] << endl;
+                arr2[i][d] = arr[i][j];                
         }
         cout << endl;
     }
-    for (int i = a; i < a; i++)
+    for (int i = 0; i < a; i++)
     {
         for (int j = 0; j < b; j++)
-            cout << arr2[i][j] << "";
+            cout << arr2[i][j] << " ";
         cout << endl;
     }
 
