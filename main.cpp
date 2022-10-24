44ad74560c40047d043cce0bfd47708c3e171949
Tiollit <Tiollit7@gmail.com>
2022-10-24 09:44:03 -0700

    Lab 12 b

--- main.cpp
+++ main.cpp
@@ -4,8 +4,25 @@
 #include <time.h>
 #include <Windows.h>
 using namespace std;
-int main()
+int Sum(int a, int b, int c)
 {
-   
+    c = 0;
+    if (a > b)
+    {
+        for (i = a; i < b; i++) c = c + i;
+    }
+    else if (a<b)
+    {
+        for (i = b; i < a; i++) c = c + i;
+    }
+    return c;
+}
+int main()
+{ 
+    int a, b, c;
+    cout << "Incert 2 numbers:" << endl;
+    cin >> a >> b;
+    c = Sum(a, b, c);
+    cout << c << endl;   
     return 0;
 }
