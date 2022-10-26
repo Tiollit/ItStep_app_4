169e70db117516225f1593311720afcc12e8a3f3
Tiollit <Tiollit7@gmail.com>
2022-10-25 21:46:46 -0700

    lab 12 done

--- main.cpp
+++ main.cpp
@@ -5,20 +5,29 @@
 #include <Windows.h>
 using namespace std;
 int perfect(int a, int b)
-{
-    cout << "Incert interval: " << endl;
-    cin >> a >> b;
-    cout << "Perfect numbers from interval is: ";
-    int c = 0;
+{     
     if (a > b)
     {
         for (int i = b; i < a; i++)
         {
-            for (int j = 1; j < i; j++)
+            int c = 0;
+            for (int j = 1; j <= i / 2; j++)
             {                
-                if (i % j == 0) ñ = ñ + j;
+                if (i % j == 0) c += j;
             }
-            if (i / c == 1) cout << i << " ";            
+            if (i == c) cout << i << " ";            
+        }
+    }
+    else if (a < b)
+    {
+        for (int i = a; i < b; i++)
+        {
+            int c = 0;
+            for (int j = 1; j <= i / 2; j++)
+            {
+                if (i % j == 0) c += j;
+            }
+            if (i == c) cout << i << " ";
         }
     }
     return 0;
@@ -26,6 +35,9 @@ int perfect(int a, int b)
 int main()
 {
     int a, b, c;
+    cout << "Incert interval: " << endl;
+    cin >> a >> b;
+    cout << "Perfect numbers from interval is: ";    
     perfect(a, b);
     return 0;
 }
