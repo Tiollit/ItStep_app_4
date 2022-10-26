4d8b7420ca36b65cf2294ea059760cfbf1a7fd6c
Tiollit <Tiollit7@gmail.com>
2022-10-25 21:25:43 -0700

    Lab 12 2d

--- main.cpp
+++ main.cpp
@@ -8,16 +8,24 @@ int perfect(int a, int b)
 {
     cout << "Incert interval: " << endl;
     cin >> a >> b;
+    cout << "Perfect numbers from interval is: ";
+    int c = 0;
     if (a > b)
     {
-        for (int i = b, i < a, i++)
+        for (int i = b; i < a; i++)
         {
-
+            for (int j = 1; j < i; j++)
+            {                
+                if (i % j == 0) ñ = ñ + j;
+            }
+            if (i / c == 1) cout << i << " ";            
         }
     }
+    return 0;
 }
 int main()
 {
-   
+    int a, b, c;
+    perfect(a, b);
     return 0;
 }
