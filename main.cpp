6c54c10daff21817f16734d6d038a70cdcff16ba
Tiollit <Tiollit7@gmail.com>
2022-10-31 16:33:13 -0700

    Lab 3 b

--- main.cpp
+++ main.cpp
@@ -1,34 +1,33 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+// Написати функцію для переведення числа, записаного у двійковому вигляді, у десяткову систему
 #include <iostream>
 #include <time.h>
 #include <Windows.h>
+#include <cmath>
 using namespace std;
-void RandArray(int arr[], const uint32_t size)
-{
-    for (size_t i = 0; i < size; i++)
+int ten(int n, int sum, int key)
+{    
+    for (int i = n; i > 0; i--)
     {
-        arr[i] = rand() % 10;
+        int temp, t;
+        t = pow(10, i - 1);
+        temp = key / t;
+        sum += temp * pow(2, i - 1);
+        key = key - temp * pow(10, i - 1); 
+        cout << temp << " " << sum << " " << key << endl;
     }
+    return sum;
 }
-void PrintArray(int arr[], const uint32_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i] << "\t";
-    }
-    cout << endl;
-}
+
 int main()
 {
-    srand(time(0));
-    const uint32_t size = 5;
-    const uint32_t size2 = 3;
-    int arr[size];
-    int arr2[size2];
-    RandArray(arr, size);
-    PrintArray(arr, size);
-    RandArray(arr2, size2);
-    PrintArray(arr2, size2);
+    int temp, t, key, n = 1, sum;
+    cout << "Incert number in 2-s sistem: " << endl;
+    cin >> key; 
+    while ((key /= 10) > 0) n++;
+    cout << n << endl;
+    sum = ten(n, 0, key);
+    cout << "The number in 10-s system: " << sum;
     return 0;
 }
