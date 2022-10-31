55c194b9cd88ecbe5b4e309c32cf75ce3e427eba
Tiollit <Tiollit7@gmail.com>
2022-10-31 09:38:56 -0700

    Lab 2 b

--- main.cpp
+++ main.cpp
@@ -1,34 +1,28 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+// Написати функцію, що реалізує алгоритм бінарного пошуку заданого ключа в одновимірному масиві.
 #include <iostream>
 #include <time.h>
 #include <Windows.h>
 using namespace std;
-void RandArray(int arr[], const uint32_t size)
+int look(int , int )
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = rand() % 10;
-    }
+   
 }
-void PrintArray(int arr[], const uint32_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i] << "\t";
-    }
-    cout << endl;
-}
+
 int main()
 {
     srand(time(0));
-    const uint32_t size = 5;
-    const uint32_t size2 = 3;
-    int arr[size];
-    int arr2[size2];
-    RandArray(arr, size);
-    PrintArray(arr, size);
-    RandArray(arr2, size2);
-    PrintArray(arr2, size2);
+    int key;
+    int arr[10];
+    int a, b, mid;   
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }    
+    cout << "Incert the looking key: " << endl;
+    cin >> key;    
+    look() ;
     return 0;
 }
